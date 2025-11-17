4a97764f554e711ab9c66008dc83a369cdaba6e8
Xavih830 <xhcg2003@gmail.com>
2025-11-16 19:22:41 -0500

    Ya casi

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 "use strict";
 
 const barraLateral = () => {
-  let barra = document.getElementById("");
+  let barra = document.getElementById("barra-lateral");
+  barra.addEventListener("click", () => {
+    let agregar = document.getElementById("agregar-lateral");
+    if (agregar.innerHTML == ""){
+      agregar.innerHTML = `
+      <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Inicio</button>
+        <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">¿Por qué nosotros?</button>
+        <button
+          class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Evaluación especializada</button>
+        <button
+          class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Contáctanos</button>
+    `;
+    } else {
+      agregar.innerHTML = "";
+    }
+  });
 }
+
+
+(() => {
+  barraLateral();
+})();

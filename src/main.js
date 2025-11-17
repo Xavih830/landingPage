6ad70d56ffba25e62114396d3ec2d516ae3a7c6a
Xavih830 <xhcg2003@gmail.com>
2025-11-17 10:46:46 -0500

    Uno dice que falta poco, pero no es asi :)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let listaBeneficios = [{1: "Te ahorramos meses de trámites y rechazos", 2: "images/ahorro_tiempo.jpg"},
-  {1: "El camión queda 100% a tu nombre", 2: "images/camion_contrato.jpg"},
-  {1: "Pagas conforme avanza el trámite, no hay riesgo", 2: "images/riesgo_cero.jpg"},
-  {1: "Compañía lista para operar en 30 días o menos", 2: "images/comp_30.jpg"},
-  {1: "Permiso de operaciones garantizado en tu ciudad", 2: "images/permiso_ciudad.jpg"},
-  {1: "No te haremos padecer de estrés, filas, ni te haremos perder tu tiempo", 2: "images/cero_estres.jpg"}
+let listaBeneficios = [{uno: "Te ahorramos meses de trámites y rechazos", dos: "images/ahorro_tiempo.jpg"},
+  {uno: "El camión queda 100% a tu nombre", dos: "images/camion_nombre.jpg"},
+  {uno: "Pagas conforme avanza el trámite, no hay riesgo", dos: "images/riesgo_cero.jpg"},
+  {uno: "Compañía lista para operar en 30 días o menos", dos: "images/comp_30.jpg"},
+  {uno: "Permiso de operaciones garantizado en tu ciudad", dos: "images/permiso_ciudad.jpg"},
+  {uno: "No te haremos padecer de estrés, filas, ni te haremos perder tu tiempo", dos: "images/cero_estres.jpg"}
 ];
 
 const barraLateral = () => {
@@ -28,9 +28,25 @@ const barraLateral = () => {
 }
 
 const cargarBeneficios = () => {
-  
+  let card = document.getElementById("beneficios");
+  card.innerHTML = "";
+  listaBeneficios.forEach(cadenas => {
+    card.innerHTML += `
+      <div class="bg-gray-900 m-1 block max-w-sm sm:max-w-150 border border-2 rounded-xl shadow-xs">
+        <img class="w-full h-55 sm:w-88 rounded-xl" src="${cadenas.dos}" alt="" />
+        <div class="p-6 text-center">
+          <h5 class="mb-4 text-1xl font-semibold tracking-tight text-heading">${cadenas.uno}</h5>
+          <button 
+            class="inline-flex items-center text-white bg-brand box-border border rounded hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+            Ver más...
+          </button>
+        </div>
+      </div>
+    `;
+  });
 }
 
 (() => {
   barraLateral();
+  cargarBeneficios();
 })();

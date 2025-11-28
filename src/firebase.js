0a2300cf41502622ad46e1207ab8b6d50a671fba
Xavih830 <xhcg2003@gmail.com>
2025-11-28 12:37:04 -0500

    Mega commit

---
+++ src/firebase.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, set, push, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const saveForm = async (email, tipoC, descrp) => {
+    const formUsers = ref(db, 'form');
+    const user = push(formUsers);
+    try {
+        await set(user, {
+            correo: email,
+            caso: tipoC,
+            contexto: descrp
+        });
+
+        return {
+            success: true,
+            message: "Todo bien"
+        };
+
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message
+        }
+    }
+};
+
+const getForm = async () => {
+    const form = ref(db, 'form');
+    try {
+        const ans = await get(form);
+
+        return {
+            success: ans.exists(),
+            data: ans.val()
+        }
+
+    } catch (error) {
+        return {
+            success: false,
+            data: "No hay datos"
+        }
+    }
+};
+
+export { saveForm, getForm };
--- src/main.js
+++ src/main.js
@@ -1,29 +1,55 @@
 "use strict";
+import { saveForm, getForm } from "./firebase";
 
-let listaBeneficios = [{uno: "Te ahorramos meses de trámites y rechazos", dos: "/assets/ahorro_tiempo.jpg"},
-  {uno: "El camión queda 100% a tu nombre", dos: "/assets/camion_nombre.jpg"},
-  {uno: "Pagas conforme avanza el trámite, no hay riesgo", dos: "/assets/riesgo_cero.jpg"},
-  {uno: "Compañía lista para operar en 30 días o menos", dos: "/assets/comp_30.jpg"},
-  {uno: "Permiso de operaciones garantizado en tu ciudad", dos: "/assets/permiso_ciudad.jpg"},
-  {uno: "No te haremos padecer de estrés, filas, ni te haremos perder tu tiempo", dos: "/assets/cero_estres.jpg"}
+let listaBeneficios = [{ uno: "Te ahorramos meses de trámites y rechazos", dos: "/assets/ahorro.png" },
+{ uno: "Pagas solo conforme avanza el trámite", dos: "/assets/confianza.png" },
+{ uno: "Compañía lista para operar en 30 días", dos: "/assets/plazo.png" },
+{ uno: "Ahorras un 77% con nosotros", dos: "/assets/precio.png" }
 ];
 
-const barraLateral = () => {
-  let barra = document.getElementById("barra-lateral");
-  barra.addEventListener("click", () => {
-    let agregar = document.getElementById("agregar-lateral");
-    if (agregar.innerHTML == ""){
-      agregar.innerHTML = `
-      <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Inicio</button>
-        <button class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">¿Por qué nosotros?</button>
-        <button
-          class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Evaluación especializada</button>
-        <button
-          class="text-gray-600 hover:bg-gray-200 w-full px-2 rounded hover:text-gray-900">Contáctanos</button>
+const resp1 = "Es una empresa especializada en la constitución legal de compañías de transporte de carga pesada en Ecuador, así como en la gestión completa del Permiso de Operaciones ante la ANT. Su servicio está enfocado en brindar un proceso 100% guiado, seguro, transparente y conforme a la normativa vigente, permitiendo que los clientes obtengan su empresa lista para operar en aproximadamente un mes, sin tener que enfrentar directamente la burocracia de las entidades públicas. Lavayen actúa como gestor legal integral, coordinando todas las etapas: factibilidad, escritura, registros, permisos y entrega final de la carpeta social completa.";
+const resp2 = `El valor total del servicio es de: USD 4.500 por cada camión. El pago se realiza de forma progresiva, conforme avanza el trámite, lo que garantiza transparencia y seguridad para el cliente: Anticipo inicial: USD 1.500. Segundo pago (con resolución de factibilidad): USD 1.000. Tercer pago (con escritura inscrita en el Registro Mercantil): USD 1.000. Cuarto pago (con registro en Superintendencia de Compañías): USD 500. Pago final (a la entrega de la carpeta social completa): USD 500. Este esquema protege al cliente, ya que solo paga cuando cada etapa oficial ha sido cumplida.`;
+const resp3 = "Lavayen opera con domicilio en la ciudad de Guayaquil, lugar estratégico donde actualmente la ANT sí está concediendo Permisos de Operaciones para compañías de transporte de carga pesada. Esto representa una ventaja clave para los clientes, ya que evita retrasos, observaciones y rechazos que suelen darse en otras ciudades del país.";
+const resp4 = "El servicio es integral, es decir, cubre todo el proceso legal y administrativo hasta dejar la compañía completamente habilitada para operar. Incluye: Aprobación del nombre de la compañía, Elaboración del proyecto económico y resolución de factibilidad, Escritura de constitución de la compañía, Nombramiento del Gerente General, Inscripción en el Registro Mercantil, Registro societario en la Superintendencia de Compañías, Asesoría para la obtención del RUC, Armado completo de la carpeta para el Permiso de Operaciones, Gestión del Permiso de Operaciones ante la ANT.";
+
+let listaPreguntas = [{ uno: "¿Qué es Lavayen?", dos: resp1 },
+{ uno: "¿Cuánto cuesta su servicio?", dos: resp2 },
+{ uno: "¿Dónde puedo encontrarlos?", dos: resp3 },
+{ uno: "¿Qué incluye su servicio?", dos: resp4 }
+];
+
+const cargarPreguntas = () => {
+  let contenedor = document.getElementById("contenedor-preguntas");
+  contenedor.innerHTML = '';
+
+  listaPreguntas.forEach(pregunta => {
+    const boton = document.createElement('button');
+    const contB = "px-5 flex fex-grow justify-between text-lg py-6 mb-2 bg-[#2d2d2d] font-semibold";
+    boton.className = contB;
+    boton.textContent = pregunta.uno;
+    const svg1 = `
+      <svg class="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
     `;
-    } else {
-      agregar.innerHTML = "";
-    }
+    const svg2 = `
+      <svg class="hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+    `;
+    boton.innerHTML += svg1;
+    boton.innerHTML += svg2;
+
+    const conten = document.createElement('div');
+    conten.className = "bg-[#2d2d2d] text-left px-5 py-6 mb-2 hidden";
+    conten.textContent = pregunta.dos;
+
+    boton.addEventListener('click', () => {
+      conten.classList.toggle("hidden");
+      boton.classList.toggle("mb-2");
+      boton.classList.toggle("mb-[1px]");
+      boton.getElementsByTagName("svg")[0].classList.toggle("hidden");
+      boton.getElementsByTagName("svg")[1].classList.toggle("hidden");
+    });
+
+    contenedor.appendChild(boton);
+    contenedor.appendChild(conten);
   });
 }
 
@@ -32,21 +58,102 @@ const cargarBeneficios = () => {
   card.innerHTML = "";
   listaBeneficios.forEach(cadena => {
     card.innerHTML += `
-      <div class="bg-gray-900 m-1 block max-w-sm sm:max-w-[365px] lg:max-w-[245px] border border-2 rounded-xl shadow-xs">
-        <img class="w-full h-55 sm:w-100 sm:h-50 lg:w-75 lg:h-34 rounded-xl" src="${cadena.dos}" alt="" />
-        <div class="p-6 text-center">
-          <h5 class="mb-4 text-1xl font-semibold tracking-tight text-heading">${cadena.uno}</h5>
-          <button 
-            class="inline-flex items-center text-white bg-brand box-border border rounded hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-            Ver más...
-          </button>
-        </div>
+      <div class="mb-2 px-3 bg-[url(${cadena.dos})] bg-center bg-no-repeat bg-cover h-48 m-1 rounded-xl">
+        <h5 class="mb-4 text-1xl pr-25 font-semibold tracking-tight text-heading">${cadena.uno}</h5>
       </div>
     `;
   });
 }
 
+const enableForm = () => {
+  const form = document.getElementById("evaluacion1");
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // 1. LOS DATOS DEBEN LEERSE AQUÍ, DENTRO DEL EVENTO
+    const tipo = document.getElementById("tipo").value;
+    const correo = document.getElementById("input_email").value;
+    const desc = document.getElementById("textarea_case").value;
+
+    // 2. LA FUNCIÓN ASÍNCRONA DEBE LLAMARSE AQUÍ
+    saveForm(correo, tipo, desc)
+      .then(exito => console.log(exito.message)) // Resuelve la Promesa externa con éxito
+      .catch(fracaso => console.log(fracaso.message)); // Resuelve la Promesa externa con fracaso
+  });
+  // 3. No se ejecuta nada más fuera de este bloque hasta que ocurra el 'submit'
+
+};
+
+const mostrarDb = () => {
+  let contenedor = document.getElementById("contenedor-datos");
+  contenedor.innerHTML = '';
+  const boton = document.createElement('button');
+  const contB = "px-5 flex fex-grow justify-between text-lg py-6 mb-2 bg-[#2d2d2d] font-semibold";
+  boton.className = contB;
+  boton.textContent = "Mostrar";
+  const svg1 = `
+      <svg class="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+    `;
+  const svg2 = `
+      <svg class="hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+    `;
+  boton.innerHTML += svg1;
+  boton.innerHTML += svg2;
+
+  const conten = document.createElement('div');
+  conten.className = "bg-[#2d2d2d] text-left px-5 py-6 mb-2 hidden";
+
+  getForm().then(result => {
+    const tabla = document.createElement('table');
+    tabla.classList = "border-separate border-spacing-x-1 border-spacing-y-[1px]";
+    tabla.innerHTML = `
+      <thead>
+        <tr>
+          <th>Email</th>
+          <th class="text-center">Tipo de caso</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+    const cuerpoT = tabla.querySelector("tbody");
+    const lista = Object.values(result.data);
+    
+    lista.forEach(fila => {
+      cuerpoT.innerHTML += `
+        <tr>
+          <td>${fila.correo}</td>
+          <td class="text-center">${fila.caso}</td>
+        </tr>
+      `;
+    });
+
+    conten.appendChild(tabla);
+
+  }).catch(e => {
+    console.log(e.data);
+  });
+  
+  boton.addEventListener('click', () => {
+    conten.classList.toggle("hidden");
+    conten.classList.toggle("flex");
+    conten.classList.toggle("flex-col");
+    conten.classList.toggle("items-center");
+    boton.classList.toggle("mb-2");
+    boton.classList.toggle("mb-[1px]");
+    boton.getElementsByTagName("svg")[0].classList.toggle("hidden");
+    boton.getElementsByTagName("svg")[1].classList.toggle("hidden");
+  });
+
+  contenedor.appendChild(boton);
+  contenedor.appendChild(conten);
+}
+
 (() => {
-  barraLateral();
+  mostrarDb();
+  enableForm();
   cargarBeneficios();
+  cargarPreguntas();
 })();
